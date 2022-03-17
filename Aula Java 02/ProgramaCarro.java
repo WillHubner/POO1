d1df6708aba7f5d68dc955d73585b79211fcc5c8
Willian Hubner <willian.hubner@hotmail.co.uk>
2022-03-16 21:47:23 -0400

    Add files via upload

---
+++ Aula Java 02/ProgramaCarro.java
@@ -0,0 +1,14 @@
+public class ProgramaCarro {
+    public static void main(String[] args){
+        
+        System.out.println("Olá");
+
+        Carro uno = new Carro();
+
+        uno.anoDeFabricacao = 2022;
+        uno.cor = "Vermelho";
+        uno.fabricante = "Fiat";
+        uno.modelo = "Way";
+    }
+
+}
